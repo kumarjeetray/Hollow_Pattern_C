c91e1ad0860318fe131d61b8e689f93c6b473b25
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-19 01:07:48 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int s;
+    printf("Enter the side of square");
+    scanf("%d",&s);
+    for(int i=1;i<=s;i++)
+    {
+        for(int j=1;j<=s;j++)
+        {
+            if(i==1||j==s||i==s||j==1)
+            {
+                printf(" ");
+                printf("*");
+            }
+            else
+            {
+                printf("  ");
+            }
+            
+        }
+        printf("\n");
+
+    }
+}
