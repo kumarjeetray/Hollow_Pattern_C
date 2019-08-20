edc067fcd2bada3a36dae75959d9df306f10c185
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-20 09:23:34 +0530

    Add files via upload

---
+++ retangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int s,l;
+    printf("Enter length of the rectangle");
+    scanf("%d",&l);
+    printf("\nEnter the side of rectangle");
+    scanf("%d",&s);
+    for(int i=1;i<=s;i++)
+    {
+        for(int j=1;j<=l;j++)
+        {
+            if(i==1 || j==1 || i==s || j==l)
+            {
+                printf(" *");
+            }
+            else
+            {
+                printf("  ");
+            }
+            
+        }
+        printf("\n");
+    }
+}
